ed10e13219bde1e7e1bfbd23f8380b2d75c36847
Peter Bagi <peter.bagi@gmail.com>
2024-12-03 14:20:32 +0100

    mouhcine

--- src/electron-root.ts
+++ src/electron-root.ts
@@ -1,86 +1,87 @@
-import * as path from 'path';
-import { app, BrowserWindow, Menu, MenuItem, shell, dialog } from 'electron';
+import { app, BrowserView, BrowserWindow } from 'electron';
 import electronLog from 'electron-log';
 
+
+let mainWindow ;
+
+
+async function launch() {
+  const singleInstanceLock = app.requestSingleInstanceLock();
+  if(!singleInstanceLock) app.quit();
+
+  app.on('second-instance',()=>{
+    if(!mainWindow) return;
+    if(mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
+  });
+
+  app.on('activate',()=>{
+    if(!BrowserWindow.getAllWindows().length) createWindow();
+  });
+
+  app.on('window-all-closed',()=>{
+    mainWindow= null;
+    app.quit();
+  })
+
+}
+
+//I think I found
+
 function createWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     height: 1000,
     width: 1800,
     title: 'Main window',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+    show:false,
   });
-  mainWindow.loadFile(path.join(__dirname, 'static/main.html'));
-  //mainWindow.webContents.openDevTools();
-}
 
-function createSecondWindow() {
-  const mainWindow = new BrowserWindow({
-    height: 800,
-    width: 800,
-    title: 'Second window',
-  });
-  mainWindow.loadFile(path.join(__dirname, 'static/second.html'));
-  //mainWindow.webContents.openDevTools();
-}
+  mainWindow.show();
 
-
-electronLog.log('requestSingleInstanceLock.');
-const gotTheLock = app.requestSingleInstanceLock();
-
-app.on('child-process-gone', (event, details) => {
-  electronLog.error('child-process-gone');
-  electronLog.error(event);
-  electronLog.error(details);
-});
-
-app.on('render-process-gone', (event, details) => {
-  electronLog.error('render-process-gone');
-  electronLog.error(event);
-  electronLog.error(details);
-});
-
-if (!gotTheLock) {
-  electronLog.log('didnt get the lock. quitting.');
-
-  app.quit();
-} else {
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
-    electronLog.log('second-instance opened');
-
-    const allWindows = BrowserWindow.getAllWindows();
-    //electronLog.log(allWindows);
-    if (allWindows.length === 0) {
-      dialog.showErrorBox(
-        'Cannot start application',
-        'Another instance of the application is running in the background.',
-      );
-      app.quit();
-      return;
+  const view = new BrowserView({
+    webPreferences:{
+      nodeIntegration: false,
+      contextIsolation: true,
     }
-    const firstWindow = allWindows[0];
-    // Someone tried to run a second instance, we should focus our existing window.
-    if (firstWindow) {
-      if (firstWindow.isMinimized()) firstWindow.restore();
-      firstWindow.focus();
-      firstWindow.show();
-      firstWindow.setVisibleOnAllWorkspaces(true);
-    }
-  });
+  })
 
-  app.whenReady().then(async () => {
-    createWindow();
+  mainWindow.setBrowserView(view);
 
-    electronLog.log('App started');
-    // The main application is bootstrapped here. Removed for demonstration.
+  view.webContents.loadURL('https://www.google.com/');
 
 
-    app.on('activate', function () {
-      // On macOS, it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
-    customizeMenu();
-  });
+  // mainWindow.loadURL('https://www.google.com/')
+  //mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', ( )=> {
+    mainWindow = null;
+  })
 }
+
+
+(async ()=> {
+
+  await app.whenReady();
+
+  await launch();
+
+})();
+
+// app.whenReady().then(async () => {
+//   createWindow();
+
+//   electronLog.log('App started');
+//   // The main application is bootstrapped here. Removed for demonstration.
+
+//   app.on('activate', function () {
+//     // On macOS, it's common to re-create a window in the app when the
+//     // dock icon is clicked and there are no other windows open.
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow();
+//   });
+// });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -90,25 +91,3 @@ app.on('window-all-closed', async () => {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-function customizeMenu() {
-  const menu = Menu.getApplicationMenu();
-
-  menu.items.forEach((item) => {
-    if (item.label === 'File') {
-      const openNewWindowItem = new MenuItem({
-        label: 'Open second window',
-        click() {
-          createSecondWindow();
-        },
-      });
-      item.submenu.append(openNewWindowItem);
-
-    }
-  });
-
-  Menu.setApplicationMenu(Menu.buildFromTemplate(menu.items));
-}
